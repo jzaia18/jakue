e5c9b24c6dace566ea9e18892834357599055728
kchen29 <kchen29@stuy.edu>
2017-03-31 12:54:41 -0400

    implement DLLDeque

--- Deque.java
+++ Deque.java
@@ -3,9 +3,9 @@ public interface Deque<T> extends Queue<T> {
     
     public void addLast(T e);
 
-    public T peekFirst(T e);
+    public T peekFirst();
 
-    public T peekLast(T e);
+    public T peekLast();
     
     public T removeFirst();
     
