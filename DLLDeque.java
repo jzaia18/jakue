4271d747854aa270c60c63fc49965ffc08846cd5
Jake Zaia <jzaia@stuy.edu>
2017-03-31 08:37:31 -0400

    Shell for DLLDeque

---
+++ DLLDeque.java
@@ -0,0 +1,52 @@
+public class DLLDeque<T> implements Deque<T> {
+
+    DLLNode<T> _front, _end;
+    int _size;
+
+    public DLLDeque() {
+
+    }
+
+    public void add( T x ) {
+    }
+
+    public T remove() {
+	return null;
+    }
+    
+    public boolean isEmpty() {
+	return true;
+    }
+
+    public T peek() {
+	return null;
+    }
+
+    public void addFirst(T e){
+
+    }
+    
+    public void addLast(T e) { 
+    }
+
+    public T peekFirst(T e) {
+	return null;
+    }
+
+    public T peekLast(T e) {
+	return null;
+    }
+    
+    public T removeFirst() {
+	return null;
+    }
+    
+    public T removeLast() {
+	return null;
+    }
+
+    public static void main (String[] args){
+
+    }
+
+}
